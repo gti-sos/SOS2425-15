d67eadbc9cdd0cef3bf39768f9b4858d55e736ce
manueljm56 <116158545+manueljm56@users.noreply.github.com>
2025-03-09 18:29:47 +0100

    tercero l04

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
   - Manuel Jiménez Martín (https://github.com/manueljm56)
   - Fernando Lagares Luna (https://github.com/FernandoLagaresLuna)
   - Gonzalo Gavilán Turiño (https://github.com/GonzaloGavilan2002)
-- **Project description**: Our project consists of analyzing the accumulated ra>
+- **Project description**: Our project consists of analyzing the accumulated rainfall of different provinces and finding the relationship with agricultural production and average temperature.
 - **Repository**: [gti-sos/SOS2425-15](https://github.com/gti-sos/SOS2425-15)
-- **URL**: TBD
+- **URL**: https://sos2425-15.onrender.com
 -  **APIs**:
     - TBD
     - TBD
--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-const express = require("express");
+const express = require("express"); 
+const path = require("path");
 const cool = require("cool-ascii-faces");
 const app = express();
 const PORT = process.env.PORT || 16078;
+const calculatePointsDeducted = require("./js/index-FLL"); // Importamos la función corregida
+const CalculateChanges = require("./js/index-GGT");
+const calculateDeceased = require("./js/index-MJM");
+const mediaAcumulaciones = require("./js/index-MJM");
 
-app.use("/",express.static("./public"));
 
-app.get("/hello",(request,response)=>{
-    response.send("Hello from the server!");
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Ruta para servir "about.html" en "/about"
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/about.html"));
 });
 
-app.get("/cool",(request,response)=>{
-    response.send(cool());
+// Ruta para "/cool"
+app.get("/cool", (req, res) => {
+    res.send(cool());
 });
 
-app.listen(PORT,()=>{
+// Nueva ruta "/samples/DLC" para ejecutar el algoritmo y devolver el resultado
+app.get("/samples/FLL", (req, res) => {
+    let ress = calculatePointsDeducted()    
+        res.send(`<h1>Resultado del cálculo</h1><p>${ress.toFixed(2)}</p>`);
+});
+
+// Nueva ruta "samples/JVF" para ejecutar el algoritmo y devolver el resultado 
+app.get("/samples/GGT", (req,res) => {
+    let respond=CalculateChanges()
+        res.send(`<h1>Resultado del cálculo</h1>${respond}<p></p>`);
+});
+
+app.get("/samples/MJM", (req,res) => {
+    let resp=mediaAcumulaciones() 
+        res.send(`<h1>Resultado del calculo</h1><p>${resp}</p>`);
+    
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}!`);
 });
