27f2af1dcdf8965be5dd1c5d3b651846080f1835
manueljm56 <116158545+manueljm56@users.noreply.github.com>
2025-04-06 23:37:26 +0200

    Corrección

--- src/back/index-MJM.js
+++ src/back/index-MJM.js
@@ -3,51 +3,28 @@ import dataStore from "nedb";
 
 const database = new dataStore();
 let precipitation_stats = [
-    { year: 2020, province: "Sevilla", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
-    { year: 2020, province: "Cádiz", annual_precipitation: 578.6, historical_average: 722.9, deviation: -144.3 },
-    { year: 2020, province: "Córdoba", annual_precipitation: 450.2, historical_average: 557.9, deviation: -107.7 },
-    { year: 2020, province: "Granada", annual_precipitation: 344.5, historical_average: 430.5, deviation: -86 },
-    { year: 2020, province: "Huelva", annual_precipitation: 617.5, historical_average: 644.6, deviation: -27.1 },
-    { year: 2020, province: "Jaén", annual_precipitation: 411.3, historical_average: 549.4, deviation: -138 },
-    { year: 2020, province: "Almería", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
-    { year: 2020, province: "Sevilla", annual_precipitation: 472, historical_average: 559.7, deviation: -87.7 },
-    { year: 2021, province: "Sevilla", annual_precipitation: 440.7, historical_average: 559.7, deviation: -119 },
-    { year: 2022, province: "Sevilla", annual_precipitation: 451.8, historical_average: 559.7, deviation: -107.9 },
-    { year: 2021, province: "Cádiz", annual_precipitation: 568.5, historical_average: 722.9, deviation: -154.3 },
-    { year: 2021, province: "Córdoba", annual_precipitation: 433.4, historical_average: 557.9, deviation: -124.5 },
-    { year: 2021, province: "Granada", annual_precipitation: 309, historical_average: 430.5, deviation: -121.5 },
-    { year: 2021, province: "Huelva", annual_precipitation: 484, historical_average: 644.6, deviation: -160.6 },
-    { year: 2021, province: "Jaén", annual_precipitation: 432.3, historical_average: 549.4, deviation: -117.1 },
-    { year: 2022, province: "Málaga", annual_precipitation: 580.4, historical_average: 628.4, deviation: -47.9 },
-    { year: 2022, province: "Cádiz", annual_precipitation: 639.2, historical_average: 722.9, deviation: -83.7 },
-    { year: 2022, province: "Córdoba", annual_precipitation: 441.2, historical_average: 557.9, deviation: -116.7 },
-    { year: 2022, province: "Granada", annual_precipitation: 372.7, historical_average: 430.5, deviation: -57.8 }    
+    { ine_code: 1, year: 2020, province: "Sevilla", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
+    { ine_code: 2, year: 2020, province: "Cádiz", annual_precipitation: 578.6, historical_average: 722.9, deviation: -144.3 },
+    { ine_code: 3, year: 2020, province: "Córdoba", annual_precipitation: 450.2, historical_average: 557.9, deviation: -107.7 },
+    { ine_code: 4, year: 2020, province: "Granada", annual_precipitation: 344.5, historical_average: 430.5, deviation: -86 },
+    { ine_code: 5, year: 2020, province: "Huelva", annual_precipitation: 617.5, historical_average: 644.6, deviation: -27.1 },
+    { ine_code: 6, year: 2020, province: "Jaén", annual_precipitation: 411.3, historical_average: 549.4, deviation: -138 },
+    { ine_code: 7, year: 2020, province: "Almería", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
+    { ine_code: 8, year: 2021, province: "Sevilla", annual_precipitation: 440.7, historical_average: 559.7, deviation: -119 },
+    { ine_code: 9, year: 2022, province: "Sevilla", annual_precipitation: 451.8, historical_average: 559.7, deviation: -107.9 },
+    { ine_code: 10, year: 2021, province: "Cádiz", annual_precipitation: 568.5, historical_average: 722.9, deviation: -154.3 },
+    { ine_code: 11, year: 2021, province: "Córdoba", annual_precipitation: 433.4, historical_average: 557.9, deviation: -124.5 },
+    { ine_code: 12, year: 2021, province: "Granada", annual_precipitation: 309, historical_average: 430.5, deviation: -121.5 },
+    { ine_code: 13, year: 2021, province: "Huelva", annual_precipitation: 484, historical_average: 644.6, deviation: -160.6 },
+    { ine_code: 14, year: 2021, province: "Jaén", annual_precipitation: 432.3, historical_average: 549.4, deviation: -117.1 },
+    { ine_code: 15, year: 2022, province: "Málaga", annual_precipitation: 580.4, historical_average: 628.4, deviation: -47.9 },
+    { ine_code: 16, year: 2022, province: "Cádiz", annual_precipitation: 639.2, historical_average: 722.9, deviation: -83.7 },
+    { ine_code: 17, year: 2022, province: "Córdoba", annual_precipitation: 441.2, historical_average: 557.9, deviation: -116.7 },
+    { ine_code: 18, year: 2022, province: "Granada", annual_precipitation: 372.7, historical_average: 430.5, deviation: -57.8 }    
 ];
 
 function loadInitialDataMJM(){
-    let precipitation_stats = [
-        { year: 2020, province: "Sevilla", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
-        { year: 2020, province: "Cádiz", annual_precipitation: 578.6, historical_average: 722.9, deviation: -144.3 },
-        { year: 2020, province: "Córdoba", annual_precipitation: 450.2, historical_average: 557.9, deviation: -107.7 },
-        { year: 2020, province: "Granada", annual_precipitation: 344.5, historical_average: 430.5, deviation: -86 },
-        { year: 2020, province: "Huelva", annual_precipitation: 617.5, historical_average: 644.6, deviation: -27.1 },
-        { year: 2020, province: "Jaén", annual_precipitation: 411.3, historical_average: 549.4, deviation: -138 },
-        { year: 2020, province: "Almería", annual_precipitation: 210.6, historical_average: 292.9, deviation: -82.4 },
-        { year: 2020, province: "Sevilla", annual_precipitation: 472, historical_average: 559.7, deviation: -87.7 },
-        { year: 2021, province: "Sevilla", annual_precipitation: 440.7, historical_average: 559.7, deviation: -119 },
-        { year: 2022, province: "Sevilla", annual_precipitation: 451.8, historical_average: 559.7, deviation: -107.9 },
-        { year: 2021, province: "Cádiz", annual_precipitation: 568.5, historical_average: 722.9, deviation: -154.3 },
-        { year: 2021, province: "Córdoba", annual_precipitation: 433.4, historical_average: 557.9, deviation: -124.5 },
-        { year: 2021, province: "Granada", annual_precipitation: 309, historical_average: 430.5, deviation: -121.5 },
-        { year: 2021, province: "Huelva", annual_precipitation: 484, historical_average: 644.6, deviation: -160.6 },
-        { year: 2021, province: "Jaén", annual_precipitation: 432.3, historical_average: 549.4, deviation: -117.1 },
-        { year: 2022, province: "Málaga", annual_precipitation: 580.4, historical_average: 628.4, deviation: -47.9 },
-        { year: 2022, province: "Cádiz", annual_precipitation: 639.2, historical_average: 722.9, deviation: -83.7 },
-        { year: 2022, province: "Córdoba", annual_precipitation: 441.2, historical_average: 557.9, deviation: -116.7 },
-        { year: 2022, province: "Granada", annual_precipitation: 372.7, historical_average: 430.5, deviation: -57.8 }    
-    ];
         
-    
     return precipitation_stats
 }
 
