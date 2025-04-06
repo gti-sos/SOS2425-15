729b79303d29c5d91d34b79f13e05f5d5545376c
Fernando Lagares Luna <fer.29.5.2004@gmail.com>
2025-04-06 23:51:39 +0200

    correccion-dato

--- src/back/index-FLL.js
+++ src/back/index-FLL.js
@@ -20,15 +20,15 @@ const ocupied_grand_stats = [
 
 const ocupied_grand_stats = [
     {ine_code: 1, year: 2021, province: "cadiz", ground: 177000, grass: 300000, wooded: 250000, non_agrarian_surface: 70000},
-    {ine_code: 1, year: 2021, province: "almería", ground: 177590, grass: 363915, wooded: 257241, non_agrarian_surface: 78557},
-    {ine_code: 1, year: 2021, province: "cordoba", ground: 728625, grass: 161589, wooded: 391269, non_agrarian_surface: 95577},
-    {ine_code: 1, year: 2020, province: "granada", ground: 534974, grass: 158569, wooded: 490865, non_agrarian_surface: 80282},
-    {ine_code: 1, year: 2019, province: "cadiz", ground: 287411, grass: 113120, wooded: 240573, non_agrarian_surface: 102517},
-    {ine_code: 1, year: 2018, province: "jaén", ground: 651284, grass: 141269, wooded: 482836, non_agrarian_surface: 74220},
-    {ine_code: 1, year: 2020, province: "sevilla", ground: 806241, grass: 137214, wooded: 313296, non_agrarian_surface: 146857},
-    {ine_code: 1, year: 2021, province: "cadiz", ground: 285947, grass: 119568, wooded: 235314, non_agrarian_surface: 103006},
-    {ine_code: 1, year: 2021, province: "caceres", ground: 397184, grass: 335124, wooded: 306463, non_agrarian_surface: 483914},
-    {ine_code: 1, year: 2018, province: "cadiz", ground: 292937, grass: 109096, wooded: 239226, non_agrarian_surface: 102365}    
+    {ine_code: 2, year: 2021, province: "almería", ground: 177590, grass: 363915, wooded: 257241, non_agrarian_surface: 78557},
+    {ine_code: 3, year: 2021, province: "cordoba", ground: 728625, grass: 161589, wooded: 391269, non_agrarian_surface: 95577},
+    {ine_code: 4, year: 2020, province: "granada", ground: 534974, grass: 158569, wooded: 490865, non_agrarian_surface: 80282},
+    {ine_code: 5, year: 2019, province: "cadiz", ground: 287411, grass: 113120, wooded: 240573, non_agrarian_surface: 102517},
+    {ine_code: 6, year: 2018, province: "jaén", ground: 651284, grass: 141269, wooded: 482836, non_agrarian_surface: 74220},
+    {ine_code: 7, year: 2020, province: "sevilla", ground: 806241, grass: 137214, wooded: 313296, non_agrarian_surface: 146857},
+    {ine_code: 8, year: 2021, province: "cadiz", ground: 285947, grass: 119568, wooded: 235314, non_agrarian_surface: 103006},
+    {ine_code: 9, year: 2021, province: "caceres", ground: 397184, grass: 335124, wooded: 306463, non_agrarian_surface: 483914},
+    {ine_code: 10, year: 2018, province: "cadiz", ground: 292937, grass: 109096, wooded: 239226, non_agrarian_surface: 102365}    
 ];
 
 
