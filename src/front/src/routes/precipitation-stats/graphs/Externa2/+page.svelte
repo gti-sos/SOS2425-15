3718daa86869aeca4c636f336aa2a8d950cab883
manueljm56 <116158545+manueljm56@users.noreply.github.com>
2025-05-11 20:58:15 +0200

    Parte Manuel

--- src/front/src/routes/precipitation-stats/graphs/Externa2/+page.svelte
+++ src/front/src/routes/precipitation-stats/graphs/Externa2/+page.svelte
@@ -1,13 +1,14 @@
-<svelte:head>
-  <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="https://code.highcharts.com/modules/variable-pie.js"></script>
-  <script src="https://code.highcharts.com/modules/exporting.js"></script>
-</svelte:head>
-
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  const punkAPI = "https://api.punkapi.com/v2/beers?page=1&per_page=10";
+  const countriesAPI = "https://restcountries.com/v3.1/all";
+
+  type SunburstNode = {
+    id: string;
+    parent?: string;
+    name: string;
+    value?: number;
+  };
 
   onMount(async () => {
     await new Promise(resolve => setTimeout(resolve, 500));
@@ -19,35 +20,93 @@
     }
 
     try {
-      const response = await fetch(punkAPI);
-      const beers = await response.json();
+      const response = await fetch(countriesAPI);
+      const countries = await response.json();
 
-      const pieData = beers.map((beer: any) => ({
-        name: beer.name,
-        y: beer.volume.value,
-        z: beer.abv
-      }));
+      const data: SunburstNode[] = [];
+      const continents = new Set<string>();
+      const subregions = new Set<string>();
+
+      countries.forEach((country: any) => {
+        const continent = country.region || 'Other';
+        const subregion = country.subregion || 'Other';
+        const countryName = country.name?.common;
+        const population = country.population || 0;
+
+        if (!continent || !subregion || !countryName) return;
+
+        continents.add(continent);
+        subregions.add(`${continent}-${subregion}`);
+
+        data.push({
+          id: countryName,
+          parent: `${continent}-${subregion}`,
+          name: countryName,
+          value: population
+        });
+      });
+
+      continents.forEach((continent) => {
+        data.push({
+          id: continent,
+          parent: '',
+          name: continent
+        });
+      });
+
+      subregions.forEach((subregionCombo) => {
+        const [continent, subregion] = subregionCombo.split('-');
+        data.push({
+          id: subregionCombo,
+          parent: continent,
+          name: subregion
+        });
+      });
 
       Highcharts.chart('container', {
         chart: {
-          type: 'variablepie'
+          height: '100%'
         },
         title: {
-          text: 'Cervezas de BrewDog: Volumen y ABV'
-        },
-        tooltip: {
-          headerFormat: '',
-          pointFormat: '<span style="color:{point.color}">\u25CF</span> <b>{point.name}</b><br/>' +
-            'Volumen: <b>{point.y} {point.volume.unit}</b><br/>' +
-            'ABV: <b>{point.z}%</b><br/>'
+          text: 'Distribución de países por continente y subregión'
         },
         series: [{
-          minPointSize: 10,
-          innerSize: '20%',
-          zMin: 0,
-          name: 'Cervezas',
-          data: pieData
-        }]
+          type: "sunburst",
+          data: data,
+          allowDrillToNode: true,
+          cursor: 'pointer',
+          dataLabels: {
+            format: '{point.name}',
+            filter: {
+              property: 'innerArcLength',
+              operator: '>',
+              value: 16
+            }
+          },
+          levels: [{
+            level: 1,
+            colorByPoint: true,
+            dataLabels: {
+              rotationMode: 'parallel'
+            }
+          }, {
+            level: 2,
+            colorByPoint: true,
+            dataLabels: {
+              rotationMode: 'parallel'
+            }
+          }, {
+            level: 3,
+            colorVariation: {
+              key: 'brightness',
+              to: -0.5
+            }
+          }]
+        }],
+        tooltip: {
+          headerFormat: "",
+          pointFormat: '<b>{point.name}</b>: {point.value} habitantes'
+        }
       });
     } catch (error) {
       console.error("Error cargando datos:", error);
@@ -55,21 +114,5 @@
   });
 </script>
 
-<figure class="highcharts-figure">
-  <div id="container" style="height: 600px;"></div>
-  <p class="highcharts-description">
-    Gráfico de pastel variable que muestra el volumen y la graduación alcohólica (ABV) de diferentes cervezas de BrewDog.
-  </p>
-</figure>
 
-<style>
-  .highcharts-figure {
-    max-width: 1000px;
-    margin: auto;
-  }
-  .highcharts-description {
-    text-align: center;
-    margin-top: 1rem;
-  }
-</style>
 
